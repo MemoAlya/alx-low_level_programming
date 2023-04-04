402dec759cb789c22558e743236e4bdedcda0573
MemoAlya <memorygwanunga@gmail.com>
2023-04-04 13:15:47 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _strpn - a function that gets the length of a prefix substring
+* _strspn - a function that gets the length of a prefix substring
 *
 * @s: the initial segment of
 *
