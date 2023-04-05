3fb1178dabcf512da5337e7a33c9201628020711
MemoAlya <memorygwanunga@gmail.com>
2023-04-05 13:15:52 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,10 +1,19 @@
 #include "main.h"
 /**
-* _print_rev_recursion - print a string in reverse
-* @s: string
-* Return 0;
-* */
-
-void _print_rev_recursion(char *s)
+* _strlen_recursion - length of string
+* @s:string
+* Return:int
+*/
+int _strlen_recursion(char *s)
 {
-if (*s)
+int i = 0;
+
+if  (*s)
+{
+i++;
+i += _strlen_recursion(s + 1);
+
+}
+return (i);
+}
+
