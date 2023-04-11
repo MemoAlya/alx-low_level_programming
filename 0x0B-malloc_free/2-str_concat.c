be03ce961a5e4546f459456ca65b1d60cd978e0a
MemoAlya <memorygwanunga@gmail.com>
2023-04-11 10:35:50 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,7 +8,7 @@
 * @s2: second string
 *
 * Return: a pointer to a newly allocated space in memory which
-* contains the contentsof s1, followed by the contents of s2,
+* contains the contents of s1, followed by the contents of s2,
 * and null terminated. NULL on failure
 */
 char *str_concat(char *s1, char *s2)
@@ -37,8 +37,8 @@ result = (char *)malloc(sizeof(char) * (len + 1));
 if (result == NULL)
 return (NULL);
 
-for (i = 0; i < len1; i++
-result[i] = s1[i]
+for (i = 0; i < len1; i++)
+result[i] = s1[i];
 for (j = 0; j < len2; j++, i++)
 result[i] = s2[j];
 result[len] = '\0';
