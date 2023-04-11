4c451b9cf05b70b1c7e41a39582d0be146d1f5ea
MemoAlya <memorygwanunga@gmail.com>
2023-04-11 16:03:24 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -14,15 +14,16 @@ char **strtow(char *str)
 {
 int i, flag, len;
 char **words;
+
 if (str == NULL || str[0] == '\0' || (str[0] == ' ' && str[1] == '\0'))
 return (NULL);
 
 i = flag = len = 0;
 while (str[i])
 {
-if (flag == 0 && str[i] != '')
+if (flag == 0 && str[i] != ' ')
 flag = 1;
-if (i > 0 && str[i] == ' ' && str[i - 1] != '')
+if (i > 0 && str[i] == ' ' && str[i - 1] != ' ')
 {
 flag =  0;
 len++;
