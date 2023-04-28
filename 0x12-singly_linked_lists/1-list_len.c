39098b2e034e27d140324931add3b4fe717086bb
MemoAlya <memorygwanunga@gmail.com>
2023-04-28 15:47:02 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,11 +4,11 @@
 #include "lists.h"
 
 /**
-* list_len - check the codd for Holberton School students.
+* list_len - check the code for Holberton School students.
 * @h: name of the list
 * Return: the number of nodes.
 */
-size_t list_len(cons list_t *h)
+size_t list_len(const list_t *h)
 {
 int count = 0;
 
