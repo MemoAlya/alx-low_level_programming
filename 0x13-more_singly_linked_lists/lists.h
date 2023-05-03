716a310393ffe15733acd320395ee834fe6a58c1
MemoAlya <memorygwanunga@gmail.com>
2023-05-03 01:52:23 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 /**
 * struct listint_s - singly linked list
-* @n: interger
+* @n: integer
 * @next: points to the next node
 *
 * Description: singly linked list node structure
@@ -28,9 +28,9 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
-size_t print_listint_safe(const listnt_t *head);
+size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
-listnt_t *find_listint_loop(listint_t *head);
+listint_t *find_listint_loop(listint_t *head);
 int _putchar(char c);
 
 #endif /*LISTS_H*/
